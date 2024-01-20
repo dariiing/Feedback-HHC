ee10679038744f51f59e53dcee38847fcb2142f8
daria <dariae9@yahoo.com>
2024-01-20 15:38:13 +0200

    add buttons for the rest of the classifiers

--- GUI.py
+++ GUI.py
@@ -32,19 +32,43 @@ class FeedbackHHCInterfaceGUI:
         self.preprocess_data_button = ttk.Button(button_frame, text="Preprocess Data", command=self.preprocess_data)
         self.preprocess_data_button.pack(pady=10, anchor="w", ipadx=5)
 
-        self.explore_data_button = ttk.Button(button_frame, text="Exploratory Analysis", command=self.explore_data_with_plot)
+        self.explore_data_button = ttk.Button(button_frame, text="Exploratory Analysis",
+                                              command=self.explore_data_with_plot)
         self.explore_data_button.pack(pady=10, anchor="w", ipadx=5)
 
-        self.select_attributes_button = ttk.Button(button_frame, text="Select Attributes using PCA", command=self.select_attributes_pca)
+        self.select_attributes_button = ttk.Button(button_frame, text="Select Attributes using PCA",
+                                                   command=self.select_attributes_pca)
         self.select_attributes_button.pack(pady=10, anchor="w", ipadx=5)
 
-        self.train_regressor_button = ttk.Button(button_frame, text="Train Random Forest Regressor", command=self.train_random_forest_regressor)
+        self.train_regressor_button = ttk.Button(button_frame, text="Train Random Forest Regressor",
+                                                 command=self.train_random_forest_regressor)
         self.train_regressor_button.pack(pady=10, anchor="w", ipadx=5)
 
-        self.train_classifier_button = ttk.Button(button_frame, text="Train Random Forest Classifier", command=self.train_random_forest_classifier)
+        self.train_classifier_button = ttk.Button(button_frame, text="Train Random Forest Classifier",
+                                                  command=self.train_random_forest_classifier)
         self.train_classifier_button.pack(pady=10, anchor="w", ipadx=5)
 
-        # Create a frame for the plot
+        self.train_svm_classifier_button = ttk.Button(button_frame, text="Train SVM Classifier",
+                                                      command=self.train_svm_classifier)
+        self.train_svm_classifier_button.pack(pady=10, anchor="w", ipadx=5)
+
+        self.train_decision_tree_classifier_button = ttk.Button(button_frame, text="Train Decision Tree Classifier",
+                                                                command=self.train_decision_tree_classifier)
+        self.train_decision_tree_classifier_button.pack(pady=10, anchor="w", ipadx=5)
+
+        self.train_neural_network_classifier_button = ttk.Button(button_frame, text="Train Neural Network Classifier",
+                                                                 command=self.train_neural_network_classifier)
+        self.train_neural_network_classifier_button.pack(pady=10, anchor="w", ipadx=5)
+
+        self.compare_models_performance_button = ttk.Button(button_frame, text="Compare Models Performance",
+                                                            command=self.compare_models_performance)
+        self.compare_models_performance_button.pack(pady=10, anchor="w", ipadx=5)
+
+        self.compare_models_performance_multiclass_button = ttk.Button(button_frame,
+                                                                       text="Compare Models Performance (Multi-Class)",
+                                                                       command=self.compare_models_performance_multiclass)
+        self.compare_models_performance_multiclass_button.pack(pady=10, anchor="w", ipadx=5)
+
         plot_frame = tk.Frame(main_frame)
         plot_frame.pack(side="right", fill="both", expand=True)
 
@@ -117,9 +141,29 @@ class FeedbackHHCInterfaceGUI:
         if self.feedback_hhc:
             self.feedback_hhc.train_random_forest_classifier()
 
+    def train_svm_classifier(self):
+        if self.feedback_hhc:
+            self.feedback_hhc.train_svm_classifier()
+
+    def train_decision_tree_classifier(self):
+        if self.feedback_hhc:
+            self.feedback_hhc.train_decision_tree_classifier()
+
+    def train_neural_network_classifier(self):
+        if self.feedback_hhc:
+            self.feedback_hhc.train_neural_network_classifier()
+
+    def compare_models_performance(self):
+        if self.feedback_hhc:
+            self.feedback_hhc.compare_models_performance()
+
+    def compare_models_performance_multiclass(self):
+        if self.feedback_hhc:
+            self.feedback_hhc.compare_models_performance_multiclass()
+
 
 if __name__ == "__main__":
     root = tk.Tk()
-    root.geometry("1000x800")
+    root.geometry("1200x800")
     app = FeedbackHHCInterfaceGUI(root)
     root.mainloop()
